050379dcbb6161785eb93998fba42654f600317a
nikitosss <nikitka.patrakov@mail.ru>
2025-02-14 08:10:36 +0300

    pravki

--- HW3_Weather/parser.c
+++ HW3_Weather/parser.c
@@ -10,23 +10,6 @@ int parse_weather_json(char* buffer)
     
     struct json_object* tokens = NULL;
 
-    json_object_object_get_ex(data, "current_condition", &tokens);
-
-    struct json_object* buf = NULL;
-
-    struct json_object* arr_item = json_object_array_get_idx(tokens, 0);
-
-    json_object_object_get_ex(arr_item, "FeelsLikeC", &buf);
-
-    printf("Температура воздуха: %.1f C\n", json_object_get_double(buf));
-
-    json_object_object_get_ex(arr_item, "windspeedKmph", &buf);
-
-    printf("Скорость ветра: %.1f км.ч\n", json_object_get_double(buf));
-
-    json_object_object_get_ex(arr_item, "winddir16Point", &buf);
-
-    printf("Направление ветра: %s\n", json_object_to_json_string(buf));
 
     return 0;
 }
